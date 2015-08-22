4c3c9b5b5f21589b62d89242f688988105a5c239
superr4y <superr4y@gmail.com>
2015-08-22 18:36:57 +0200

    check if conf.tar.gz exist

--- make_release.sh
+++ make_release.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-tar -czf system-config/conf.tar.gz system-config/*
+SYSTEM_CONFIG=${SYSTEM_CONFIG:-"./system-config/conf.tar.gz"}
+
+if [ -f "${SYSTEM_CONFIG}" ]; then
+    rm "${SYSTEM_CONFIG}"
+fi
+tar -czf "${SYSTEM_CONFIG}" system-config/*
